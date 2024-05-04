52aea9603c6ff7887f48bcbf5e6fd8065ebedc25
ju-lit-hein <julien.ferdinand@epitech.eu>
2024-05-04 23:18:42 +0400

    feat: set good position for the Bar

--- Bar.ts
+++ Bar.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import Object from './Object';
+import { PI } from 'three/examples/jsm/nodes/Nodes.js';
 
 class bar extends Object {
     constructor(position: THREE.BoxGeometry) {
@@ -8,11 +9,15 @@ class bar extends Object {
 
     public async load(scene: THREE.Scene) {
         await super.loadModel("ressource/bar.glb", scene);
-        this.object.position.z = -10;
-        this.object.position.y = 4;
-        this.object.rotation.y = 0
-        this.object.rotation.x = 0
-        this.object.rotation.z = 0
+        this.object.position.z = -20;
+        this.object.position.y = 20;
+        this.object.position.x = 0;
+        this.object.rotation.y = 3.14;
+        this.object.rotation.x = 0;
+        this.object.rotation.z = 0;
+        this.object.scale.x = 7;
+        this.object.scale.y = 7;
+        this.object.scale.z = 7;
     }
 
     update(delta: number) {
--- main.js
+++ main.js
@@ -8,8 +8,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 15;
-camera.position.y = 3.5;
+camera.position.z = 25;
+camera.position.y = -5;
+camera.rotateX(0.5);
 const sceneManager = new SceneManager(camera);
 sceneManager.addScene(sceneStart);
 sceneManager.addScene(sceneComposer);
@@ -18,14 +19,15 @@ sceneManager.changeScene("composer");
 console.log(sceneManager.scenes)
 function onDocumentKeyDown(event) {
     var keyCode = event.key;
-    sceneComposer.objects[sceneComposer.index].get().position.x = 0;
-    sceneComposer.objects[sceneComposer.index].get().rotation.x = 0;
+    console.log(sceneComposer.ingredients);
+    sceneComposer.ingredients[sceneComposer.index].get().position.x = 0;
+    sceneComposer.ingredients[sceneComposer.index].get().rotation.x = 0;
     if (keyCode == "ArrowUp") {
-        sceneComposer.index = sceneComposer.index == sceneComposer.objects.length - 1 ? 0 : sceneComposer.index + 1;
+        sceneComposer.index = sceneComposer.index == sceneComposer.ingredients.length - 1 ? 0 : sceneComposer.index + 1;
     } else if (keyCode == "ArrowDown") {
-        sceneComposer.index = sceneComposer.index == 0 ? sceneComposer.objects.length - 1 : sceneComposer.index - 1;
+        sceneComposer.index = sceneComposer.index == 0 ? sceneComposer.ingredients.length - 1 : sceneComposer.index - 1;
     }
-    sceneComposer.objects[sceneComposer.index].get().position.x = 1;
+    sceneComposer.ingredients[sceneComposer.index].get().position.x = 1;
 }
 
 function launchAnimation(event) {
@@ -41,7 +43,7 @@ document.addEventListener("keydown", onDocumentKeyDown, false);
 function animate() {
     requestAnimationFrame(animate);
     sceneManager.update();
-    console.log(sceneManager.getCurrentScene().getScene())
+    // console.log(sceneManager.getCurrentScene().getScene())
     renderer.render(sceneManager.getCurrentScene().getScene(), camera);
 }
 
